90a3fd33f12009d21c9cb25c38ef7513a8416432
wooden5730 <wooden5730@msn.com>
2026-08-28 00:13:48 -0400

    Updated Footer

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,4 +1,5 @@
 import React from 'react'; 
+import '../App.css'
 const Footer = () => {
 
     return ( 
@@ -11,16 +12,23 @@ const Footer = () => {
   <div class="footer-section">
     <p>Built with <strong>Vite and React and semantic HTML</strong></p>
   </div>
-
-  <div class="footer-section">
+  
+    <div class="footer-section">
     <h3>Follow me:</h3>
-    <div class="social-links">
-     <ul>
-      <li><a href="#">GitHub</a></li>
-      <li><a href="#">LinkedIn</a></li>
-      <li><a href="#">Twitter</a></li>
-     </ul>
-    </div>
+    <div class="social-links">   
+      <a href="https://www.linkedin.com/in/michael-wooden-165a7411b/" target="_blank">
+       <img src="./src/linkedin.png" alt="Linkedin" />
+      </a>      
+      <a href="https://www.facebook.com" target="_blank">
+        <img src="./src/facebook-logo.png" alt="Facebook" /> 
+      </a>
+       <a href="https://x.com/" target="_blank">
+        <img src="./src/twitter.png" alt="Twitter" />
+       </a>
+       <a href="https://www.youtube.com" target="_blank">
+        <img src="./src/youtube.png" alt="YouTube" />
+    </a>
+     </div>
   </div>
   <div class="column">
       <h3>Contact</h3>
